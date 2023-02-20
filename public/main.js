5f7bea07f0ebcf9ac409e04a8d5f92f137598a3a
Thuy Nguyen <vanthuy101102@gmail.com>
2023-02-20 21:52:02 +0700

    simple filter product

---
+++ public/main.js
@@ -0,0 +1,101 @@
+let menuFood = [
+  {
+    name: "Bun Ca Cay",
+    img: "./public/image/img-1.jpg",
+    rating: "20 reviews",
+    type: "breakfast",
+    price: "$60",
+  },
+  {
+    name: "Banh Mi Sua",
+    img: "./public/image/img-2.jpg",
+    rating: "60 reviews",
+    type: "breakfast",
+    price: "$30",
+  },
+  {
+    name: "Xoi Xeo Ruoc",
+    img: "./public/image/img-3.jpg",
+    rating: "40 reviews",
+    type: "breakfast",
+    price: "$15",
+  },
+  {
+    name: "Buffalo Steak",
+    img: "./public/image/img-4.jpg",
+    rating: "200 reviews",
+    type: "dinner",
+    price: "$30",
+  },
+  {
+    name: "Vit Quay Bac Kinh",
+    img: "./public/image/img-5.jpg",
+    rating: "100 reviews",
+    type: "dinner",
+    price: "$60",
+  },
+  {
+    name: "Ga Luoc Mam Ot",
+    img: "./public/image/img-6.jpg",
+    rating: "20 reviews",
+    type: "dinner",
+    price: "$80",
+  },
+  {
+    name: "Nuoc Ep Cam",
+    img: "./public/image/img-7.jpg",
+    rating: "75 reviews",
+    type: "drink",
+    price: "$15",
+  },
+  {
+    name: "Nuoc Oi Dao",
+    img: "./public/image/img-8.jpg",
+    rating: "50 reviews",
+    type: "drink",
+    price: "$18",
+  },
+];
+let food = document.querySelector(".food");
+let btns = document.querySelectorAll("button");
+
+function render(menu) {
+  let results = menu.map((item) => {
+    return `<div class="menu-card">
+        <div class="card-top">
+          <img src=${item.img} alt="food" />
+          <span class="type">${item.type}</span>
+        </div>
+        <div class="card-content">
+          <h3 class="card-head">${item.name}</h3>
+          <div class="card-review">
+            <div class="card-star">
+              <img src="./public/image/Shape.png" alt="" />
+              <span class="card-rating">5.8</span>
+            </div>
+            <p class="card-count">${item.rating}</p>
+          </div>
+          <a href="#!" class="card-price">Order only ${item.price}</a>
+        </div>
+      </div>`;
+  });
+  food.innerHTML = results.join("");
+}
+//render ra views
+render(menuFood);
+
+let foodFilter = menuFood;
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let output = foodFilter.filter((item) => {
+      if (item.type === btn.className) {
+        return item;
+      }
+    });
+    render(output);
+    if (btn.className === "all") {
+      render(foodFilter);
+    }
+  });
+});
